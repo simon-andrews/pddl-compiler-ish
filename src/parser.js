952898688ff4ed3fea9a889237e55693e838ed3e
Simon Andrews <simon.andrews212@gmail.com>
2018-12-21 00:15:36 -0500

    Add wrapper that adds parens around parsers

--- src/parser.js
+++ src/parser.js
@@ -10,28 +10,34 @@ function word(str) {
   return P.string(str).thru(token);
 }
 
+// takes a parser, and returns a parser that requires parentheses () around it
+function withParens(parser) {
+  return word("(").then(parser).skip(word(")"));
+}
+
 let PDDL = P.createLanguage({
 
   lparen: () => word("("),
   rparen: () => word(")"),
 
   Domain: function(r) {
-    return r.lparen
-      .then(word("define"))
+    return withParens(
+      word("define")
       .then(r.lparen)
       .then(P.seq(
         word("domain"),
         r.Name.skip(r.rparen),
         r.ExtensionDef.atMost(1)
-      ))
-      .skip(r.rparen);
+      )));
   },
 
   ExtensionDef: function(r) {
-    return r.lparen.then(P.seq(
+    return withParens(
+      P.seq(
         word(":extends"),
         r.Name
-    )).skip(r.rparen);
+      )
+    );
   },
 
   // Spec reference: McDermott 1998, page 7
