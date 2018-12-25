198fc4986cc7a258c100f6153616790f10f3c36d
Simon Andrews <simon.andrews212@gmail.com>
2018-12-25 00:20:15 -0500

    Remove domain extension unit tests + fix others

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ var assert = require("assert");
 var parser = require("../src/parser.js");
 
 describe("Parser", function() {
-  
+
   describe("Domain definitions", function() {
-    
+
     it("should be able to handle multiple domains in a single file", function() {
       let text = `(define (domain gripper))
       (define (domain other-gripper))
@@ -20,34 +20,13 @@ describe("Parser", function() {
       `;
       let ast = parser.parse(text);
       assert.equal(1, ast.length);
-      assert.equal("wacky-whitespace", ast[0][1]);
-    });
-
-    describe("Extension definitions", function() {
-
-      it("can handle more than one extension", function() {
-        let text = `
-        (define (domain test)
-          (:extends bleep bloop))
-        `;
-        let ast = parser.parse(text);
-        let extendsBit = ast[0][2][0][1];
-        assert.equal(JSON.stringify(["bleep", "bloop"]), JSON.stringify(extendsBit));
-      });
-
-      it("still handles one extension correctly", function() {
-        let text = "(define (domain test) (:extends beep))";
-        let ast = parser.parse(text);
-        let extendsBit = ast[0][2][0][1];
-        assert.equal(JSON.stringify(["beep"]), JSON.stringify(extendsBit));
-      });
-
+      assert.equal("wacky-whitespace", ast[0].name);
     });
 
   });
 
   it("can handle files starting with whitespace", function() {
-    let text = `   
+    let text = `
     (define (domain test))
     `;
     let ast = parser.parse(text);
